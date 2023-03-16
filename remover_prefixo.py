b5179138f75fccf10671efe8f76df8c5ad0ceed4
Franklin Albuquerque <126638424+franklin-albuquerque@users.noreply.github.com>
2023-03-16 13:31:23 -0300

    Atualizar remover_prefixo.py

--- remover_prefixo.py
+++ remover_prefixo.py
@@ -1,21 +1,16 @@
-import os
+from os import walk, path, rename
 
 def remover_prefixo(prefixo):
     local = '.'
 
-    for pasta, _, arquivo in os.walk(local):
-        for arquivo in arquivo:
-
+    for _, _, arquivos in walk(local):
+        for arquivo in arquivos:
             if prefixo in arquivo:
                 nome_original = arquivo
                 novo_nome = arquivo.split(prefixo)[1]
 
-                if novo_nome != nome_original:
-                    antigo_caminho = os.path.join(pasta, nome_original)
-                    novo_caminho = os.path.join(pasta, novo_nome)
-
-                    if not os.path.isfile(novo_caminho):
-                        os.rename(antigo_caminho, novo_caminho)
+                if novo_nome != nome_original and not path.isfile(path.join(local, novo_nome)):
+                    rename(path.join(path.join(local, nome_original), path.join(local, novo_nome)))
 
 def main():
     prefixo = input('Digite o prefixo que deseja remover dos arquivos: ')
